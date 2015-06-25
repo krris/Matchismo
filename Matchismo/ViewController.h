02091310a27e83804ad730c289654cca8c8ad0a5
krris <krrisgit@gmail.com>
2015-06-25 19:39:11 +0200

    Initial Commit

---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by krris on 25.06.2015.
+//  Copyright (c) 2015 krris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Matchismo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by krris on 25.06.2015.
+//  Copyright (c) 2015 krris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
