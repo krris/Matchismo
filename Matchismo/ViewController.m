cc7f3287ee3a1b4957773917b73a68ea263ab88f
krris <krrisgit@gmail.com>
2015-07-20 22:50:18 +0200

    Small fixes.

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -13,7 +13,7 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-@property Deck *deck;
+@property (nonatomic, strong) Deck *deck;
 @end
 
 @implementation ViewController
@@ -30,20 +30,28 @@
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
                           forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
+        self.flipCount++;
     } else {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:UIControlStateNormal];
         Card* randomCard = [[self deck] drawRandomCard];
         NSLog(@"Random card: %@", [randomCard contents]);
-        [sender setTitle:[randomCard contents] forState:UIControlStateNormal];
+        if (randomCard) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:[randomCard contents] forState:UIControlStateNormal];
+            self.flipCount++;
+        }
     }
-    self.flipCount++;
 }
 
 - (Deck *) deck
 {
-    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    if (!_deck) _deck = [self createDeck];
     return _deck;
+}
+
+- (Deck *) createDeck
+{
+    return [[PlayingCardDeck alloc] init];
 }
 
 @end
